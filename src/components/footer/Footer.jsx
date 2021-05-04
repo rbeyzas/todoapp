e632c85763929b66914058a04507baf19484db3b
rbeyzas <rukiyebeyzasarikaya@hotmail.com>
2021-05-04 15:47:04 +0300

    last update

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import './Footer.css';
+
+const Footer = () => {
+	return (
+		<div class="footer-basic">
+			<footer>
+				<div class="social">
+					<a href="https://github.com/rbeyzas">
+						<i class="fa fa-github"></i>
+					</a>
+					<a href="#">
+						<i class="icon ion-social-snapchat"></i>
+					</a>
+					<a href="#">
+						<i class="icon ion-social-twitter"></i>
+					</a>
+					<a href="#">
+						<i class="icon ion-social-facebook"></i>
+					</a>
+				</div>
+				<p class="text-center">Rukiye Beyza Sarıkaya | Web Developer </p>
+			</footer>
+		</div>
+	);
+};
+
+export default Footer;
